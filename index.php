d3a359d1b9597047fac4702d37536a5a3a0b6539
Johan Steffner <johan@agigen.se>
2013-12-22 21:45:15 +0100

    skrollr test

--- index.php
+++ index.php
@@ -39,8 +39,8 @@ require 'events.php';
         ?>
         <section class="agigen-section"
         <?php if (isset($event->img)): ?> style="background-image: url(/img/events/<?php echo $event->img ?>)" <?php endif ?> 
-        data-top-top="filter: grayscale(0);"
-        data-top-bottom="filter: grayscale(1);"
+        data-center-center="filter: grayscale(0%);"
+        data-top-center="filter: grayscale(100%);"
         >
             <div class="agigen-section__container vc-container">
                 <div class="vc">
