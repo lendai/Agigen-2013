453bc7968dd7c7e6c83b6caf9f5d454564d6a9ab
Daniel Mauno Pettersson <daniel@agigen.se>
2013-12-22 19:09:52 +0100

    struktur ish

---
+++ events.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+
+    
+    hockeymaskin
+    mia började
+    
+    carlsberg
+    hobbit
+    
+    byta kontor
+    
+    inflyttningsfest
+    
+    oscar
+    therese
+    victor
+    
+    
+    omsättning
+    
+    
+    
+*/
+
+$events = array(
+    
+    (object)array(
+        'title' => 'hockeymaskin'
+    ),
+    (object)array(
+        'title' => 'miriam började'
+    ),
+    (object)array(
+        'title' => 'carlsberg'
+    ),
+    (object)array(
+        'title' => 'hobbit'
+    ),
+    (object)array(
+        'title' => 'nytt kontor'
+    ),
+    (object)array(
+        'title' => 'inflyttningsfest'
+    ),
+    (object)array(
+        'title' => 'oscar började'
+    ),
+    (object)array(
+        'title' => 'therese började'
+    ),
+    (object)array(
+        'title' => 'victor började'
+    ),
+    (object)array(
+        'title' => 'omsättning ökade'
+    ),
+    (object)array(
+        'title' => '2014 = ???? ???  PROFIT'
+    ),
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+);
+
+
+
+?>
