b1dcc03af3784ab485eab484555edcb7c5b33678
Daniel Mauno Pettersson <daniel@agigen.se>
2013-12-22 20:24:42 +0100

    content

--- events.php
+++ events.php
@@ -1,44 +1,26 @@
 <?php
-
-/*
-
-    
-    hockeymaskin
-    mia började
-    
-    carlsberg
-    hobbit
-    
-    byta kontor
-    
-    inflyttningsfest
-    
-    oscar
-    therese
-    victor
-    
-    
-    omsättning
-    
-    
-    
-*/
-
 $events = array(
-    
     (object)array(
         'title' => "World's first hockey puck shooting machine",
         'when' => 'February 2013'
     ),
+    
     (object)array(
-        'title' => 'the flixies',
-        'when' => 'March 2013'
+        'title' => 'The Big Blog Exchange',
+        'when' => 'February 2013',
+        'video' => 'http://www.youtube.com/watch?v=hos7_HYxGeE',
+        'link' => 'http://www.bigblogexchange.org/',
+        'img' => 'bigblog.gif'
+
     ),
     
-    // (object)array(
-    //     'title' => 'ny dörr',
-    //     'when' => 'March 2013'
-    // ),
+    (object)array(
+        'title' => 'Netflix - The Flixies',
+        'when' => 'March 2013',
+        'video' => 'http://www.youtube.com/watch?v=v5NtNn43NW4',
+        'img' => 'flixies.jpg'
+        
+    ),
     (object)array(
         'title' => 'bear.sh',
         'when' => 'March 2013'
@@ -47,9 +29,19 @@ $events = array(
         'title' => 'Miriam joined Agigen',
         'when' => 'March 2013'
     ),
+    
+    (object)array(
+        'award' => true,
+        'title' => 'Webby Awards - Best online campaign 2012: Build with Chrome',
+        'when' => 'May 2:nd',
+        'link' => 'http://winners.webbyawards.com/2013/interactive-advertising-media/campaign-categories/online-campaigns/build-with-chrome'
+    ),
+    
+    
     (object)array(
         'title' => "Carlsberg - Where's the party featuring Axwell",
-        'when' => 'May 10:th'
+        'when' => 'May 10:th',
+        'img' => 'carlsberg.jpg'
     ),
     (object)array(
         'title' => 'Released Shroomify',
@@ -81,13 +73,11 @@ $events = array(
         'link' => 'http://www.thefwa.com/site/where-s-the-party-by-carlsberg',
         'when' => 'September 2013',
     ),
-    
-    (object)array(
-        'title' => 'Featured in Swedish Startup Space',
-        'when' => 'September 2013',
-        'link' => 'http://swedishstartupspace.com/2013/09/18/agigen-lean-startup-shop/'
-    ),
-    
+    // (object)array(
+    //     'title' => 'Featured in Swedish Startup Space',
+    //     'when' => 'September 2013',
+    //     'link' => 'http://swedishstartupspace.com/2013/09/18/agigen-lean-startup-shop/'
+    // ),
     (object)array(
         'title' => 'New office!',
         'when' => 'October 2013',
