a69a8d7f9e909ba7327a7173ce835970617bee2c
Johan Steffner <johan@agigen.se>
2013-12-22 22:06:05 +0100

    to make skrollr work with devices

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-skrollr.init({
-    forceHeight: false,
-    smoothScrolling: false
-})
+(function($, window, undefined) {
+    "use strict";
+
+    $(window).on('resize', function() {
+        $('.agigen-section').height(window.innerHeight)
+    }).trigger('resize')
+
+    skrollr.init({
+        forceHeight: false,
+        smoothScrolling: false
+    })
+})(jQuery, window);
